ed2ff92a846b6be22f2678007efd9190d9ba904f
phuongha2012 <hami201289@him-mail.ch>
2020-05-22 10:37:49 +1200

    set up client folder

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const keys = require('./config/keys');
 
-const PORT = process.env.PORT || 3000;
-
 // CONNECT TO DATABASE WITH MONGOOSE
 const mongodbURI = `mongodb+srv://${keys.MONGO_USER}:${keys.MONGO_PASSWORD}@${keys.MONGO_CLUSTER_NAME}-qc9dp.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -20,7 +18,7 @@ mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Connected to MongoDB');
+  console.log('Connecting to DB');
 });
 
 // APP SET-UPS
@@ -29,7 +27,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static('client'));
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,4 +36,20 @@ app.use(cors());
 require('./routes/studentsRoutes')(app);
 require('./routes/projectsRoutes')(app);
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+// SERVE BUILT FRONTEND IN PRODUCTION ENV
+if (process.env.NODE_ENV === 'production') {
+
+    // serve fully-built version
+    app.use(express.static('client/build'));
+
+    // fallback to serve index.html if a route is not recognised
+    const path = requrie('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
