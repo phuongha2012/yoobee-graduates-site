813f86cd6273778598d578e360852a14a7ce78ef
phuongha2012 <hami201289@him-mail.ch>
2020-05-25 15:33:03 +1200

    further setup for components and scss styling

---
+++ client/src/components/App/App.js
@@ -0,0 +1,12 @@
+  import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="testClass">
+      Welcome to our graduate site!
+    </div>
+  );
+}
+
+export default App;
