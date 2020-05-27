37aec907615986cd8fe06770e4628c88d6d7bda3
phuongha2012 <hami201289@him-mail.ch>
2020-05-27 12:19:01 +1200

    login got response from server

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
+import axios from 'axios';
+import UserReducer from '../../contexts/UserReducer';
 
 const LoginPage = () => {
+    const [hasLoginError, setHasLoginError] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [state, dispatch] = useReducer(UserReducer);
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/login', { username, password })
+            .then(response => {
+                console.log(response);
+            })
+    };
+
+    const onInputChange = setter => e => {
+        setter(e.target.value);
+      };
+
+
     return (
-        <div>
-            Login
-        </div>
-    )
+        <form className="login-form" onSubmit={onSubmitHandler}>
+            <h1>Login Page</h1>
+            {hasLoginError && (
+                <div className="login-form-error">
+                Login Failed: Incorrect Credentials
+                </div>
+            )}
+            <fieldset>
+                <label>
+                <span>Username</span>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={onInputChange(setUsername)}
+                    placeholder="dev"
+                    required
+                />
+                </label>
+                <label>
+                <span>Password</span>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={onInputChange(setPassword)}
+                    placeholder="password"
+                    required
+                />
+                </label>
+            </fieldset>
+        <button>Login</button>
+    </form>
+    );
 }
 
 export default LoginPage;
--- client/src/contexts/UserReducer.js
+++ client/src/contexts/UserReducer.js
@@ -1,14 +1,14 @@
 const UserReducer = (state, action) => {
-    switch (action.type) {
-        case 'login': {
-            const { username, password } = action.payload;
-            return {
-                ...state,
-                hasLoginError: false,
-                user
-            }
-        }
-    }
+    // switch (action.type) {
+    //     case 'login': {
+    //         const { username, password } = action.payload;
+    //         return {
+    //             ...state,
+    //             hasLoginError: false,
+    //             user
+    //         }
+    //     }
+    // }
 }
 
 export default UserReducer;
