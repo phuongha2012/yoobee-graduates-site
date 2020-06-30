f38385ea343134fd0751f7e0dd87d536a593bf06
Sean <seanreilly123@hotmail.com>
2020-06-30 23:06:59 +1200

    Merged up to date hopefully

--- client/src/components/ManageAccount/ProjectCard.js
+++ client/src/components/ManageAccount/ProjectCard.js
@@ -1,18 +1,8 @@
 import React from "react";
 import "./ManageAccount.scss";
 
-<<<<<<< HEAD
 const ProjectCard = ({ project }) => {
     return <div className="card-grid">{project.title}</div>;
 };
-=======
-const ProjectCard = (props) => {
-    return (
-        <div>
-            Project Card
-        </div>
-    )
-}
->>>>>>> 7db0116f3def17a36983849eda60a18c6e49af13
 
 export default ProjectCard;
