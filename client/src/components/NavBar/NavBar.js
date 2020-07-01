9e68a1a2b63721feb4ecc3111b7eca45001c37ef
Sean <seanreilly123@hotmail.com>
2020-07-02 01:11:57 +1200

    Navbar over top of headings, home buttons opaque

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,47 +1,18 @@
 import React, { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
-import "../App/App.scss";
+import "./NavBar.scss";
 
 const NavBar = () => {
-    const userContext = useContext(UserContext);
-    const history = useHistory();
-
-    const logoutHandler = () => {
-        userContext.logout();
-        history.push("/");
-    };
-
     return (
-        <div className="navbar d-flex justify-content-around bg-light">
-            <div className="col-md-3">
-                <Link to="/">Logo/Home</Link>
-            </div>
-            <div className="col-md-6 d-flex justify-content-around">
+        <nav className="d-flex align-items-center justify-content-between">
+            <Link to="/">Logo</Link>
+            <div className="col-12 col-sm-4 d-flex justify-content-between">
                 <Link to="/students">Students</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/">About</Link>
             </div>
-            <div className="col-md-3">
-                {userContext.state.user ? (
-                    <div className="d-flex justify-content-around">
-                        <div>
-                            <Link to="/account">My Account</Link>
-                        </div>
-                        <div onClick={logoutHandler}>Logout</div>
-                    </div>
-                ) : (
-                    <div className="d-flex justify-content-end">
-                        <div>
-                            <Link to="/login">Login</Link>
-                        </div>
-                        <div>
-                            <Link to="/register">Register</Link>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </div>
+        </nav>
     );
 };
 
