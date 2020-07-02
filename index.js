47c8c2efc29181241abd1c03c342f947585c303d
phuongha2012 <hami201289@him-mail.ch>
2020-07-03 08:20:08 +1200

    fix heroku log error

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     // fallback to serve index.html if a route is not recognised
-    const path = requrie("path");
+    const path = require("path");
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
