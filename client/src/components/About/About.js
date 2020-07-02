688021081ae719f97385b13e3899a6af5598326a
Sean Reilly <seanreilly123@hotmail.com>
2020-07-02 14:26:36 +1200

    About page added and linked

---
+++ client/src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+    return (
+        <div className="heading-banner about-banner text-center">
+            <h1 className="single-heading">About the exhibition</h1>
+        </div>
+    );
+};
+
+export default About;
